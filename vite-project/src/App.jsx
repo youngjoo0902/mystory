4ded41e28749509f9f07c80ab7c599ce424960d8
leeyoungjoo <youngjoo0902@naver.com>
2026-06-06 17:50:13 +0900

    최초 init

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  /*
+    ★구현할거★
+    - 깃 연결, 깃 페이지 연결
+    - 회원가입
+    - 로그인 페이지
+    - 라우터 적용
+  */
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
